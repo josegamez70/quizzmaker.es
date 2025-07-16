285de53d95e3bac1484cf7870adfa71ddfc3cb28
josegamez70 <josegamez70@hotmail.com>
2025-07-16 12:43:36 +0200

    Refactor: Mover proyecto a la raíz para simplificar build

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,3 @@
-// vite.config.ts
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  
-  // Le decimos a Vite que no intente empaquetar esta librería,
-  // ya que causa problemas en el entorno de Netlify.
-  build: {
-    rollupOptions: {
-      external: [
-        '@google/generative-ai'
-      ]
-    }
-  }
-})
+export default defineConfig({ plugins: [react()] })
