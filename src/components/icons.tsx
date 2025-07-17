d2f8f3cbc47949fbdd77c1bdbbc9274c6f49e9a1
josegamez70 <josegamez70@hotmail.com>
2025-07-17 10:51:25 +0200

    Fix: Exporta UploadIcon y FileTextIcon en icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -120,6 +120,7 @@ export const InformationCircleIcon = (props: React.SVGProps<SVGSVGElement>) => (
   </svg>
 );
 
+// Icono de Bombilla (Lightbulb)
 export const LightbulbIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -230,7 +231,6 @@ export const LogoutIcon = (props: React.SVGProps<SVGSVGElement>) => (
   </svg>
 );
 
-// Icono de Casa (Home) - ¡El que faltaba!
 export const HomeIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg 
     xmlns="http://www.w3.org/2000/svg" 
@@ -246,5 +246,47 @@ export const HomeIcon = (props: React.SVGProps<SVGSVGElement>) => (
   >
     <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
     <polyline points="9 22 9 12 15 12 15 22"></polyline>
+  </svg>
+);
+
+// Icono de Subir Archivo (Upload) - ¡El que faltaba!
+export const UploadIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    width="24" 
+    height="24" 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    stroke="currentColor" 
+    strokeWidth="2" 
+    strokeLinecap="round" 
+    strokeLinejoin="round" 
+    {...props}
+  >
+    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+    <polyline points="17 8 12 3 7 8"></polyline>
+    <line x1="12" y1="3" x2="12" y2="15"></line>
+  </svg>
+);
+
+// Icono de Texto/Documento (FileText) - También importado en ImageUploader.tsx
+export const FileTextIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    width="24" 
+    height="24" 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    stroke="currentColor" 
+    strokeWidth="2" 
+    strokeLinecap="round" 
+    strokeLinejoin="round" 
+    {...props}
+  >
+    <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+    <polyline points="14 2 14 8 20 8"></polyline>
+    <line x1="16" y1="13" x2="8" y2="13"></line>
+    <line x1="16" y1="17" x2="8" y2="17"></line>
+    <polyline points="10 9 9 9 8 9"></polyline>
   </svg>
 );
